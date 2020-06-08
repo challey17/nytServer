47f83fd5e3cc0b415bfdcd4ae8278bf7bfc65377
Chayce Knaub <chknaub@yahoo.com>
2020-06-07 21:32:48 -0400

    booksearch server

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("common")); // let's see what 'common' format looks like
+
+const books = require("./books-data");
+
+app.get("/books", (req, res) => {
+  const { search = "", sort } = req.query;
+
+  if (sort) {
+    if (!["title", "rank"].includes(sort)) {
+      return res.status(400).send("Sort must be one of title or rank");
+    }
+  }
+
+  let results = books.filter((book) =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  res.json(results);
+});
+
+app.listen(8000, () => {
+  console.log("Server started on PORT 8000");
+});
